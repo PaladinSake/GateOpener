856d30555a1fc03961f4a9c7293e2885cdb5cf0e
Ryan Denton <denton.ryanb@gmail.com>
2020-03-25 07:12:20 +0000

    Test Script finalised. Protoype plate checking script developed.

---
+++ ALPR.py
@@ -0,0 +1,182 @@
+from openalpr import Alpr
+from argparse import ArgumentParser
+from PIL import Image
+from pathlib import Path
+import time
+#import RPi.GPIO as GPIO
+import os
+import shutil
+import time
+import re
+import json
+
+print("imports done.")
+
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(11, GPIO.IN)
+#GPIO.setup(3, GPIO.OUT)
+
+print('GPIO setup complete.')
+
+def convert_to_greyscale(file_path):
+    splitpath = os.path.split(file_path)
+    directory = splitpath[0]
+    
+    img = Image.open(file_path).convert('LA')
+    output_file = directory + '/greyscale_' + Path(file_path).stem + '.png'
+    img.save(output_file)
+    print('file saved as: ' + output_file)
+            
+def resize_image(file_path):
+    splitpath = os.path.split(file_path)
+    directory = splitpath[0]
+    
+    basewidth = 300
+    img = Image.open(file_path)
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    
+    output_file = directory + '/resized_' + Path(file_path).stem + '.png'
+    img.save(output_file)
+    print('file saved as: ' + output_file)
+    
+
+def process_image(file_path):
+
+    alpr.set_top_n(1)
+    #alpr.set_default_region("au")
+    alpr.set_detect_region(True)
+    jpeg_bytes = open(file_path, "rb").read()
+    results = alpr.recognize_array(jpeg_bytes)
+
+    # Uncomment to see the full results structure
+    # import pprint
+    # pprint.pprint(results)
+
+    print("Image size: %dx%d" %(results['img_width'], results['img_height']))
+    print("Processing Time: %f" % results['processing_time_ms'])
+
+    i = 0
+    for plate in results['results']:
+        i += 1
+        for candidate in plate['candidates']:
+            prefix = "-"
+            if candidate['matches_template']:
+                prefix = "*"
+
+            print 'Plate: ' + candidate['plate'], candidate['confidence']
+            
+    return candidate['plate']
+
+def capture_image():
+    shutil.copyfile("ImageSource/ea7the.jpg", basepath + "/CapturedImage.jpg")
+    
+def archive_images():
+    time_string = time.strftime("%Y%m%d-%H%M%S")
+    
+    for dir_item in os.listdir(basepath):
+        
+        fullpath = os.path.join(basepath, dir_item)
+        
+        if os.path.isdir(fullpath):
+            #Skip directory items.
+            print('skip directory item.')
+        elif not os.path.isdir(fullpath):
+            print('Moving ' + fullpath + ' to Archive...')
+            shutil.move(basepath + "/"+dir_item, basepath + "/Archive/" + time_string + "-" + dir_item)
+            
+def validate_access(vehicle_plate):
+    print('validating access...')
+    
+    permission = False
+    
+    with open('ApprovedPlates.json') as access_list:
+        data = access_list.read()
+        
+    print('data defined.')
+        
+    obj = json.loads(data)
+    
+    for approved_plate in obj["Numberplate List"]:
+        #print('checking against: ' + approved_plate)
+        
+        if vehicle_plate == approved_plate['Plate']:
+           permission = True
+        
+        
+    return permission
+    
+alpr = None
+try:
+    
+    country = 'us'
+    config = '/etc/openalpr/openalpr.conf'
+    runtime_data = '/usr/share/openalpr/runtime_data'
+    basepath = 'Images'
+    
+    i=0
+    
+    print("begin try")
+    alpr = Alpr(country, config, runtime_data)
+    
+    plate_image = basepath + "/CapturedImage.jpg"
+    
+    print("Using OpenALPR " + alpr.get_version())
+
+    if not alpr.is_loaded():
+        print("Error loading OpenALPR")
+    else:
+        print("ALPR Loaded.")
+        
+        #while True:
+        while i<=10:
+            
+            #input = GPIO.input(11)
+            #if input == 0:
+            if i<10:
+                print "No Movement."
+                #GPIO.output(3, 0) # Turn light off.
+            #elif input == 1:
+            elif i==10:
+                print('Movement Detected')
+                #GPIO.output(3,1) # Turn light on.
+                
+                capture_image()
+            
+                filename = (Path(plate_image).stem)
+                
+                print('converting to greyscale...')
+                convert_to_greyscale(plate_image)
+                
+                print("Resizing Greyscale...")
+                resize_image(basepath + '/greyscale_' + filename + '.png')
+                
+                print("Processing Resized Greyscale...")
+                numberplate_output = process_image(basepath + '/resized_greyscale_' + filename + '.png')
+                
+                if re.search('[0-9]', numberplate_output):
+                    print('digit found.')
+                else:
+                    print('nothing')
+                    
+                approval = validate_access(numberplate_output)
+                
+                if approval == True:
+                    print 'Access Granted. Opening Gate...'
+                else:
+                    print 'Access Denied. Manual Gate open Required.'
+                
+                archive_images()
+            
+            time.sleep(0.1)
+            
+            i=i+1
+        
+
+finally:
+    if alpr:
+        alpr.unload()
+
+    print("End finally.")
